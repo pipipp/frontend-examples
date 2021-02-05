d705658ef17d94284b243e4ed024eb2ddbd1e418
evaliu <1872863416@qq.com>
2021-02-05 18:06:54 +0800

    update

--- examples/javascript/example.js
+++ examples/javascript/example.js
@@ -1,6 +1,6 @@
 /* 输出
-window.alert('')       弹出警告框
-document.write('')     将内容写到HTML文档中(!!!如果在文档加载后执行，会导致文档覆盖)
-innerHTML=''           写入到HTML元素
-console.log('')        写入到浏览器的控制台
+window.alert("")       写入内容到弹出警告框
+document.write("")     写入内容到HTML文档(!!!如果在文档加载后执行，会导致文档覆盖)
+innerHTML=""           写入内容到HTML元素
+console.log("")        写入内容到浏览器的控制台(用于调试)
 */
