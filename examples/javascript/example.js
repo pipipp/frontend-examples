f4477cb637fe3986c11c028f903dbb2a645ddad6
evaliu <1872863416@qq.com>
2021-02-06 09:28:06 +0800

    update

--- examples/javascript/example.js
+++ examples/javascript/example.js
@@ -1,6 +1,25 @@
+/* Javascript Basic Examples */
+
 /* 输出
 window.alert("")       写入内容到弹出警告框
+console.log("")        写入内容到浏览器的控制台(用于调试)
 document.write("")     写入内容到HTML文档(!!!如果在文档加载后执行，会导致文档覆盖)
 innerHTML=""           写入内容到HTML元素
-console.log("")        写入内容到浏览器的控制台(用于调试)
+*/
+
+/* 语句
+var	                声明一个变量
+function	        定义一个函数
+while	            当条件语句为 true 时，执行语句块
+do ... while	    执行一个语句块，在条件语句为 true 时继续执行该语句块(至少执行一次)
+for	                在条件语句为 true 时，可以将代码块执行指定的次数
+for ... in	        用于遍历数组或者对象的属性（对数组或者对象的属性进行循环操作）
+switch	            用于基于不同的条件来执行不同的动作
+continue	        跳过循环中的一个迭代
+break	            用于跳出循环
+if ... else	        用于基于不同的条件来执行不同的动作
+return	            退出函数
+try	                实现错误处理，与 catch 一同使用
+catch	            语句块，在 try 语句块执行出错时执行 catch 语句块
+throw	            抛出（生成）错误
 */
